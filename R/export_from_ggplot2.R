87b25ae82c5602706a7fff98362005eb3b0d892e
Jamie <jameswgriffith@gmail.com>
2022-06-19 23:00:31 -0500

    Tweaks to documentation

--- R/export_from_ggplot2.R
+++ R/export_from_ggplot2.R
@@ -1,4 +1,8 @@
-#' lurn functions exported from ggplot2
+#' Functions exported from ggplot2
+#'
+#' @description autoplot() is exported so that it can be used, even if ggplot2
+#' is not loaded. Even so, we recommend loading ggplot2 using
+#'  \code{library(ggplot2)}.
 #'
 #' @importFrom ggplot2 autoplot
 #' @name autoplot
